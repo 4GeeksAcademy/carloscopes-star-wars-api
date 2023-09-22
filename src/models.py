37dbe7c3beaa111e0c34c290bf72cee299a276c0
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-09-22 02:03:12 +0000

    Modelos de entidades User, Character, Planet, FavoriteCharacter y FavoritePlanet definidos

--- src/models.py
+++ src/models.py
@@ -17,3 +17,97 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.String(50), nullable=False)
+    mass = db.Column(db.String(50), nullable=False)
+    hair_color = db.Column(db.String(50), nullable=False)
+    skin_color = db.Column(db.String(50), nullable=False)
+    eye_color = db.Column(db.String(50), nullable=False)
+    birth_year = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
+    img = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Character {self.name}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "img": self.img,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    diameter = db.Column(db.String(50), nullable=False)
+    gravity = db.Column(db.String(50), nullable=False)
+    population = db.Column(db.String(50), nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
+    terrain = db.Column(db.String(50), nullable=False)
+    surface_water = db.Column(db.String(50), nullable=False)
+    img = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Planet {self.name}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "img": self.img,
+        }
+
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+    character_id = db.Column(db.Integer(), db.ForeignKey("character.id"))
+    user = db.relationship("User")
+    character = db.relationship("Character")
+
+    def __repr__(self):
+        return f"<Favorite {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+        }
+
+class FavoritePlanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+    planet_id = db.Column(db.Integer(), db.ForeignKey("planet.id"))
+    user = db.relationship("User")
+    planet = db.relationship("Planet")
+
+    def __repr__(self):
+        return f"<Favorite {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+        }
